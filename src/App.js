966040559a14f2eb56e13bd0954644e2cbe15cc3
Ol-create <oluola96@gmail.com>
2022-08-20 22:28:58 -0600

    Creat route for component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter as Rout,
+         Switch,
+         Route,
+} from "react-router-dom"
+
 import './App.css';
 import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
 
 function App() {
   return (
     <div className="App">
-    <Header />
+    <Rout>
+        <Header />
+        <Switch>
+        <Route path="/" exact component= {ProductListing} />
+        <Route path="/product/:productId" exact component= {ProductDetail} />
+        <Route>404 Not Found </Route>
+        </Switch>
+    </Rout>
       <h1>Hello</h1>
     </div>
   );
